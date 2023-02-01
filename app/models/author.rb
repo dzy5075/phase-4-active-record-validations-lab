d0841dba33a43565dd432d024efe196fb53fe878
Daniel Yim <daniel.yim96@gmail.com>
2023-02-01 11:29:20 -0500

    commit tree 53498f0c00b0b40b2518cff55bc215cfbb8945f4

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,10 @@
 class Author < ApplicationRecord
+validates :name, {
+    presence: true, 
+    uniqueness: true
+}
+validates :phone_number, length { is: 10}
+
+validates :is_phone_number_format
+# if this is the correct format
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, { :length => { :minimum => 250} }
+    validates :summary, { :length => { :maximum => 250} }
+    validates :boolean_field_name, inclusion: ['Fiction', 'Non-Fiction']
+
+    validates :is_clickbaity 
+
+    def is_clickbaity
+        # if this is NOT clickbaity enough
+        #i.e if this title does not contain "Won't Beleive"
+        if !(title.include? "'Won't Beleive")
+        byebug
+            # add an error message to the /errors object
+            erros.add(:title, 'hey this is not clickbaity enough')
+    end
+
 end
